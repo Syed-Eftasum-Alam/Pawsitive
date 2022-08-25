ab4f36c26598ff6c64f011af70c709e6903017c5
Ahsan Khan <30434801+Ahsan40@users.noreply.github.com>
2022-08-25 21:46:14 +0600

    added animal profile view (partial)

--- src/main/java/com/example/petadoption/CatsSectionController.java
+++ src/main/java/com/example/petadoption/CatsSectionController.java
@@ -8,9 +8,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -47,6 +51,10 @@ public class CatsSectionController implements Initializable {
     @FXML
     private Rectangle profile6;
 
+//     Data
+    private ArrayList<Animal> list;
+
+
     @FXML
     void exit(MouseEvent event) {
         System.exit(0);
@@ -64,6 +72,7 @@ public class CatsSectionController implements Initializable {
         Parent root= FXMLLoader.load(getClass().getResource("Profile.fxml")) ;
 
         Scene scene=new Scene(root);
+        HelloApplication.makeDraggable(scene);
         HelloApplication.primaryStage.setScene(scene);
         HelloApplication.primaryStage.show();
 
@@ -74,6 +83,7 @@ public class CatsSectionController implements Initializable {
         Parent root= FXMLLoader.load(getClass().getResource("Sign1st.fxml")) ;
 
         Scene scene=new Scene(root);
+        HelloApplication.makeDraggable(scene);
         HelloApplication.primaryStage.setScene(scene);
         HelloApplication.primaryStage.show();
 
@@ -83,36 +93,54 @@ public class CatsSectionController implements Initializable {
         Parent root= FXMLLoader.load(getClass().getResource("RegisteredPETS.fxml")) ;
 
         Scene scene=new Scene(root);
+        HelloApplication.makeDraggable(scene);
         HelloApplication.primaryStage.setScene(scene);
         HelloApplication.primaryStage.show();
-
     }
+
     public void switchtoSceneHelloview(ActionEvent e)throws IOException {
         Parent root= FXMLLoader.load(getClass().getResource("hello-view.fxml")) ;
 
         Scene scene=new Scene(root);
+        HelloApplication.makeDraggable(scene);
         HelloApplication.primaryStage.setScene(scene);
         HelloApplication.primaryStage.show();
-
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Init();
     }
-    private void Init(){
-        ArrayList<Animal>cats= new ArrayList<>();
-        try{
-            Scanner sc= new Scanner("cat.txt");
-            String line="";
-            while (sc.hasNext()){
-                line=sc.nextLine();
 
+    private Animal getAnimal(String str) {
+        String[] p = str.split("##");
+        return new Animal(p[0], p[1], p[2], p[3], p[4], p[5], p[6]);
+    }
+
+    private void readData(String path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line;
+            while((line=br.readLine()) != null) {
+                list.add(getAnimal(line));
             }
+            br.close();
+        } catch (Exception e) {}
+    }
 
+    private void Init(){
+        list = new ArrayList<>();
+        readData("cat.txt");
+        readData("dog.txt");
+
+        // array
+        Rectangle[] rectangles = {profile1, profile2, profile3, profile4, profile5, profile6};
+        for(int i = list.size(); i < 6; i++) {
+            rectangles[i].setVisible(false);
         }
-        catch (IOException e){}
 
-
+        for(int i = 0; i < list.size(); i++) {
+            rectangles[i].setFill(new ImagePattern(new Image("file:" + list.get(i).getAnimalPic())));
+        }
     }
 }
